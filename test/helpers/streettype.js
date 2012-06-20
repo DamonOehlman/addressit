3218628624a461ad1cc8728b80097703ed2077b6
Damon Oehlman <damon.oehlman@sidelab.com>
2012-06-20 10:26:23 +1000

    Refinements to supported street types to avoid collisions with Australian suburb names

---
+++ test/helpers/streettype.js
@@ -0,0 +1,19 @@
+var addressit = require('../../'),
+    expect = require('expect.js');
+
+var streetType = function(locale) {
+    return function(type) {
+        it('supports street type: ' + type, function() {
+            var input = '15 FOO ' + type + ' BARVILLE',
+                address = addressit(input, locale);
+                
+            expect(address.number).to.equal(15);
+            expect(address.street).to.equal('FOO ' + type.toUpperCase());
+            expect(address.regions).to.eql(['BARVILLE']);
+        });
+    };
+};
+
+if (typeof module != 'undefined') {
+    module.exports = streetType;
+}
--- test/parse-en.js
+++ test/parse-en.js
@@ -1,4 +1,5 @@
-var canParse = require('./helpers/canparse')('en');
+var canParse = require('./helpers/canparse')('en'),
+    streetType = require('./helpers/streettype')('en');
 
 describe('street parsing (EN locale)', function() {
     canParse('2649 Logan Road, Eight Mile Plains, QLD', {
@@ -40,11 +41,11 @@ describe('street parsing (EN locale)', function() {
         "regions": ["PERTH"]
     });
     
-    canParse('1/135 Ferny Way, Ferny Hills', {
+    canParse('1/135 Ferny Way, Ferny Grove', {
         "unit": 1,
         "number": 135,
         "street": "FERNY WAY",
-        "regions": ["FERNY HILLS"]
+        "regions": ["FERNY GROVE"]
     });
     
     canParse('Shop 8, 431 St Kilda Rd Melbourne', {
@@ -74,4 +75,107 @@ describe('street parsing (EN locale)', function() {
         "street": "ST KILDA ROAD",
         "regions": ["MELBOURNE", "VIC"]
     });
+
+    streetType('ALLEY');
+    streetType('ALLY');
+    streetType('APPROACH');
+    streetType('APP');
+    streetType('ARCADE');
+    streetType('ARC');
+    streetType('AVENUE');
+    streetType('AV');
+    streetType('AVE');
+    streetType('BOULEVARD');
+    streetType('BLVD');
+    streetType('BROW');
+    streetType('BYPASS');
+    streetType('BYPA');
+    streetType('CAUSEWAY');
+    streetType('CWAY');
+    streetType('CIRCUIT');
+    streetType('CCT');
+    streetType('CIRCUS');
+    streetType('CIRC');
+    streetType('CLOSE');
+    streetType('CL');
+    streetType('COPSE');
+    streetType('CPSE');
+    streetType('CORNER');
+    streetType('CNR');
+    // streetType('COVE');
+    streetType('COURT');
+    streetType('CT');
+    streetType('CRESCENT');
+    streetType('CRES');
+    streetType('DRIVE');
+    streetType('DR');
+    // streetType('END');
+    streetType('ESPLANANDE');
+    streetType('ESP');
+    // streetType('FLAT');
+    streetType('FREEWAY');
+    streetType('FWAY');
+    streetType('FRONTAGE');
+    streetType('FRNT');
+    // streetType('GARDENS');
+    // streetType('GDNS');
+    streetType('GLADE');
+    streetType('GLD');
+    // streetType('GLEN');
+    streetType('GREEN');
+    streetType('GRN');
+    // streetType('GROVE');
+    // streetType('GR');
+    // streetType('HEIGHTS');
+    // streetType('HTS');
+    streetType('HIGHWAY');
+    streetType('HWY');
+    streetType('LANE');
+    streetType('LN');
+    streetType('LINK');
+    streetType('LOOP');
+    streetType('MALL');
+    streetType('MEWS');
+    streetType('PACKET');
+    streetType('PCKT');
+    streetType('PARADE');
+    streetType('PDE');
+    // streetType('PARK');
+    streetType('PARKWAY');
+    streetType('PKWY');
+    streetType('PLACE');
+    streetType('PL');
+    streetType('PROMENADE');
+    streetType('PROM');
+    streetType('RESERVE');
+    streetType('RES');
+    // streetType('RIDGE');
+    // streetType('RDGE');
+    streetType('RISE');
+    streetType('ROAD');
+    streetType('RD');
+    streetType('ROW');
+    streetType('SQUARE');
+    streetType('SQ');
+    streetType('STREET');
+    streetType('ST');
+    streetType('STRIP');
+    streetType('STRP');
+    streetType('TARN');
+    streetType('TERRACE');
+    streetType('TCE');
+    streetType('THOROUGHFARE');
+    streetType('TFRE');
+    streetType('TRACK');
+    streetType('TRAC');
+    streetType('TRUNKWAY');
+    streetType('TWAY');
+    // streetType('VIEW');
+    streetType('VISTA');
+    streetType('VSTA');
+    streetType('WALK');
+    streetType('WAY');
+    streetType('WALKWAY');
+    streetType('WWAY');
+    streetType('YARD');
 });
