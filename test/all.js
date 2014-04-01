aced66bacbbdd421152f5aeee017ae94648a1851
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-02 06:59:28 +1100

    Complete case preservation tests

--- test/all.js
+++ test/all.js
@@ -1,2 +1,3 @@
 require('./parse');
+require('./parse-preservecase');
 require('./street-types');
--- test/parse-preservecase.js
+++ test/parse-preservecase.js
@@ -10,70 +10,70 @@ test('2649 Logan Road, Eight Mile Plains, QLD', expect({
   regions: ['Eight Mile Plains', 'QLD']
 }));
 
-// test('2649 Logan Road Eight Mile Plains, QLD', expect({
-//   number: 2649,
-//   street: 'LOGAN ROAD',
-//   regions: ['EIGHT MILE PLAINS', 'QLD']
-// }));
+test('2649 Logan Road Eight Mile Plains, QLD', expect({
+  number: 2649,
+  street: 'Logan Road',
+  regions: ['Eight Mile Plains', 'QLD']
+}));
 
-// test('4 N 2nd St #950, San Jose, CA', expect({
-//   unit: 950,
-//   number: 4,
-//   street: "N 2ND ST",
-//   regions: ["SAN JOSE", "CA"]
-// }));
+test('4 N 2nd St #950, San Jose, CA', expect({
+  unit: 950,
+  number: 4,
+  street: "N 2nd St",
+  regions: ["San Jose", "CA"]
+}));
 
-// test('1 Queen Street, Brisbane', expect({
-//   "number": 1,
-//   "street": "QUEEN STREET",
-//   "regions": ["BRISBANE"]
-// }));
+test('1 Queen Street, Brisbane', expect({
+  "number": 1,
+  "street": "Queen Street",
+  "regions": ["Brisbane"]
+}));
 
-// test('754 Robinson Rd West, Aspley, QLD', expect({
-//   number: 754,
-//   street: 'ROBINSON RD WEST',
-//   regions: ['ASPLEY', 'QLD']
-// }));
+test('754 Robinson Rd West, Aspley, QLD', expect({
+  number: 754,
+  street: 'Robinson Rd West',
+  regions: ['Aspley', 'QLD']
+}));
 
-// test('Sydney', expect({
-//   "regions": ["SYDNEY"]
-// }));
+test('Sydney', expect({
+  "regions": ["Sydney"]
+}));
 
-// test('Perth', expect({
-//   "regions": ["PERTH"]
-// }));
+test('Perth', expect({
+  "regions": ["Perth"]
+}));
 
-// test('1/135 Ferny Way, Ferny Grove', expect({
-//   "unit": 1,
-//   "number": 135,
-//   "street": "FERNY WAY",
-//   "regions": ["FERNY GROVE"]
-// }));
+test('1/135 Ferny Way, Ferny Grove', expect({
+  "unit": 1,
+  "number": 135,
+  "street": "Ferny Way",
+  "regions": ["Ferny Grove"]
+}));
 
-// test('Shop 8, 431 St Kilda Rd Melbourne', expect({
-//   "unit": 8,
-//   "number": 431,
-//   "street": "ST KILDA RD",
-//   "regions": ["MELBOURNE"]
-// }));
+test('Shop 8, 431 St Kilda Rd Melbourne', expect({
+  "unit": 8,
+  "number": 431,
+  "street": "St Kilda Rd",
+  "regions": ["Melbourne"]
+}));
 
-// test('Eight Mile Plains', expect({
-//   "regions": ["EIGHT MILE PLAINS"]
-// }));
+test('Eight Mile Plains', expect({
+  "regions": ["Eight Mile Plains"]
+}));
 
-// test('St George', expect({
-//   "regions": ["ST GEORGE"]
-// }));
+test('St George', expect({
+  "regions": ["St George"]
+}));
 
-// test('3N751 Hawthorn Dr., St. Charles, IL', expect({
-//   "number": "3N751",
-//   "street": "HAWTHORN DR",
-//   "regions": ["ST CHARLES", "IL"]
-// }));
+test('3N751 Hawthorn Dr., St. Charles, IL', expect({
+  "number": "3N751",
+  "street": "Hawthorn Dr",
+  "regions": ["St Charles", "IL"]
+}));
 
-// test('8/437 St Kilda Road Melbourne, VIC', expect({
-//   "unit": 8,
-//   "number": 437,
-//   "street": "ST KILDA ROAD",
-//   "regions": ["MELBOURNE", "VIC"]
-// }));
+test('8/437 St Kilda Road Melbourne, VIC', expect({
+  "unit": 8,
+  "number": 437,
+  "street": "St Kilda Road",
+  "regions": ["Melbourne", "VIC"]
+}));
