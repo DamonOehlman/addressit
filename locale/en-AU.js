15c3dc47091c20ec5e38e3375437df9268072eac
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-16 22:43:36 +1000

    Initial implementation of en-AU postcode parsing

--- locale/en-AU.js
+++ locale/en-AU.js
@@ -1,9 +1,23 @@
 var parser = require('../parsers/en.js');
+var rePostcode = /(\d{4})\s*$/;
 
 module.exports = function(input, opts) {
   // parse the base address
-  var address = parser(input, opts);
+  var address = parser(input, opts).finalize();
+
+  // iterate through the regions and look for postcode parts
+  address.regions = address.regions.map(function(part) {
+    var match = rePostcode.exec(part);
+
+    if (match) {
+      part = part.slice(0, match.index).trim();
+      address.postalcode = parseInt(match[1], 10);
+    }
+
+    return part;
+  });
+
 
   // look for any postcode parts amongst the remaining regions
-  return address.finalize();
+  return address;
 };
